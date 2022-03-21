b414c6dcbc8c5120dbafdce628c1ce0d3375cb7f
Yusong Shi <jackshi.sys@gmail.com>
2022-03-21 15:55:49 -0400

    Getting Started

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const fruits = require('./fruits')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.listen(
+    PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`)
+    )
