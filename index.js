2496a8de913bab9da43ba604e59ba5c79b6a3fac
Mark Harmon <neothej1sec@gmail.com>
2022-03-21 14:44:57 -0500

    added part 2 setting up routes

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ const app = express()
 app.listen(PORT, () => {
     console.log(`Serving up delicious fruits on port ${PORT} 🍒`)
 })
+
+app.get('/ping', (req, res) => {res.json('pong')})
+
+app.get('/greet/:name', (req, res) => {
+    res.send({msg: `Why hello there, ${req.params}`})
+})
+
+app.get('/evens/:n', (req, res) => {
+    res.send([parseInt(2,4,6,8,10)])
+})
+
+app.get('/namelength/:name', (req, res) => {
+    res.send({
+        msg: `${req.params.name} has ${req.params.name.length} letters in their name`
+    })
+})
+
