2ee2ae43a7b098211656d42557afe10dd09481e8
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-18 17:42:13 -0500

    even array return

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ app.get(
     res.send(five)
   }
 )
+app.get(
+  '/evens/:n',
+  (req, res, next) => {
+    next()
+  },
+  (req, res) => {
+    let evenArr = []
+    for (let i = 2; i <= parseInt(req.params.n); i += 2) evenArr.push(i)
+    console.log(evenArr)
+    res.send(evenArr)
+  }
+)
 
 app.listen(PORT, () =>
   console.log(`Serving up delicious fruits on port ${PORT} 🍒`)
