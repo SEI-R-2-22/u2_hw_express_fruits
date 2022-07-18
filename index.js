71ec73cbbaf515b6e75f857ab89b54e6b796b6b4
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-18 18:05:02 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -8,72 +8,36 @@ app.get('/ping', (req, res) => {
   res.json('pong')
 })
 
-app.get(
-  '/greet/:name',
-  (request, response, next) => {
-    next()
-  },
-  (req, res) => {
-    res.send(`Why hello there, ${req.params.name}!`)
-  }
-)
-app.get(
-  '/five',
-  (request, response, next) => {
-    next()
-  },
-  (req, res) => {
-    let five = [1, 2, 3, 4, 5]
-    res.send(five)
-  }
-)
-app.get(
-  '/evens/:n',
-  (req, res, next) => {
-    next()
-  },
-  (req, res) => {
-    let evenArr = []
-    for (let i = 2; i <= parseInt(req.params.n); i += 2) evenArr.push(i)
-    console.log(evenArr)
-    res.send(evenArr)
-  }
-)
+app.get('/greet/:name', (req, res) => {
+  res.send(`Why hello there, ${req.params.name}!`)
+})
+app.get('/five', (req, res) => {
+  let five = [1, 2, 3, 4, 5]
+  res.send(five)
+})
+app.get('/evens/:n', (req, res) => {
+  let evenArr = []
+  for (let i = 2; i <= parseInt(req.params.n); i += 2) evenArr.push(i)
+  console.log(evenArr)
+  res.send(evenArr)
+})
 
-app.get(
-  '/namelength/:name',
-  (req, res, next) => {
-    next()
-  },
-  (req, res) => {
-    let nameLength = req.params.name
-    res.send(`${nameLength.length}`)
-  }
-)
+app.get('/namelength/:name', (req, res) => {
+  let nameLength = req.params.name
+  res.send(`${nameLength.length}`)
+})
 
-app.get(
-  '/fruits',
-  (req, res, next) => {
-    next()
-  },
-  (req, res) => {
-    res.send(fruits)
-  }
-)
+app.get('/fruits', (req, res) => {
+  res.send(fruits)
+})
 
-app.get(
-  '/fruits/:name',
-  (req, res, next) => {
-    next()
-  },
-  (req, res) => {
-    let fruitsName = fruits.find(
-      ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
-    )
+app.get('/fruits/:name', (req, res) => {
+  let fruitsName = fruits.find(
+    ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
+  )
 
-    res.send(fruitsName)
-  }
-)
+  res.send(fruitsName)
+})
 
 app.listen(PORT, () =>
   console.log(`Serving up delicious fruits on port ${PORT} 🍒`)
